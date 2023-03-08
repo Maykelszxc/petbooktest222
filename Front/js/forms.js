fb8e8a23eea889f94cb56ab845a1bda92110f9e8
Maykelszxc <84752645+Maykelszxc@users.noreply.github.com>
2023-03-09 00:07:18 +0800

    Add files via upload

---
+++ Front/js/forms.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const signupLink = document.querySelector('.signup-link');
+const btnPopup = document.querySelector('.btn-Login-Popup');
+const iconClose = document.querySelector('.icon-close');
+
+
+signupLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
+
+
+
